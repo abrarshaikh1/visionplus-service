8cbd97ce5313979d73a6f58fcd4fe56822925c5e
Abrar107 <abrarshaikh1071@gmail.com>
2024-07-25 12:10:41 +0530

    first commit

---
+++ src/main/java/com/visionplus/visionplus/VisionplusApplication.java
@@ -0,0 +1,13 @@
+package com.visionplus.visionplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisionplusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisionplusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/visionplus/visionplus/VisionplusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.visionplus.visionplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisionplusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
